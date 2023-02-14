3e264bf15fcc3e163b6a2e9d74865c135eaad816
semihgullboy <smhglby17@gmail.com>
2023-02-14 19:44:33 +0300

    Create Odev1.sql

---
+++ Odev1.sql
@@ -0,0 +1,5 @@
+--1. SORU SELECT title ,description FROM film;
+--2. SORU SELECT * FROM film WHERE length >60 AND length <75;
+--3. SORU SELECT * FROM film WHERE rental_rate =0.99 AND (replacement_cost =12.99 OR replacement_cost  =28.99);
+--4. SORU SELECT last_name FROM customer WHERE first_name = 'Mary';
+--5. SORU SELECT * FROM film Where length < 50 AND NOT (rental_rate =2.99 OR rental_rate =4.99);
